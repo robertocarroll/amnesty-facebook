5787632c19c4fcff1753201e0c7c5b4090332f0e
robertocarroll <rob@wearethoughtfox.com>
2017-04-05 21:44:58 +0400

    Script vendor copy command.

---
+++ scripts/copy-vendor-js.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp node_modules/jquery/dist/jquery.min.js app/vendor/jquery.min.js;
+cp node_modules/underscore/underscore-min.js app/vendor/underscore-min.js;
+cp node_modules/backbone/backbone-min.js app/vendor/backbone-min.js;
