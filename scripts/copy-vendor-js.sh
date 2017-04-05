3f43455a7c51d0694ec967cf0d460d19cd8aff91
robertocarroll <rob@wearethoughtfox.com>
2017-04-05 21:49:19 +0400

    Finished the vendor copying script.

--- scripts/copy-vendor-js.sh
+++ scripts/copy-vendor-js.sh
@@ -3,3 +3,5 @@
 cp node_modules/jquery/dist/jquery.min.js app/vendor/jquery.min.js;
 cp node_modules/underscore/underscore-min.js app/vendor/underscore-min.js;
 cp node_modules/backbone/backbone-min.js app/vendor/backbone-min.js;
+cp node_modules/backbone.radio/build/backbone.radio.min.js app/vendor/backbone.radio.min.js;
+cp node_modules/backbone.marionette/lib/backbone.marionette.min.js app/vendor/backbone.marionette.min.js;
