5bac590d2a36891bc19b674c8e6337b3fbeca4e4
robertocarroll <rob@wearethoughtfox.com>
2017-04-05 23:46:15 +0400

    Loading text strings into a template.

--- app/data/strings.js
+++ app/data/strings.js
@@ -1,11 +1,11 @@
 var textStrings = {
   "LANG" : "En",
-  "1NOMINATE" : "Nominate",
-  "1NOMINATE_DESC" : "Experienced BRAVE? Celebrate BRAVE people in your life: log in to nominate them now.",
-  "1NOMINATE_BTN" : "Nominate with Facebook",
-  "2NOMINATE_BTN" : "Back",
-  "2NOMINATE_CALL" : "Select your BRAVE Facebook friends",
-  "3NOMINATE_CALL" : "Share why you think they're BRAVE",
-  "3NOMINATE_BTN" : "Back",
-  "3NOMINATE_BTN_2" : "Submit"
+  "_1NOMINATE" : "Nominate",
+  "_1NOMINATE_DESC" : "Experienced BRAVE? Celebrate BRAVE people in your life: log in to nominate them now.",
+  "_1NOMINATE_BTN" : "Nominate with Facebook",
+  "_2NOMINATE_BTN" : "Back",
+  "_2NOMINATE_CALL" : "Select your BRAVE Facebook friends",
+  "_3NOMINATE_CALL" : "Share why you think they're BRAVE",
+  "_3NOMINATE_BTN" : "Back",
+  "_3NOMINATE_BTN_2" : "Submit"
 };
--- app/views/hello.js
+++ app/views/hello.js
@@ -1,10 +1,9 @@
 var HelloWorld = Backbone.Marionette.View.extend({
   el: '#app',
-  template: _.template('<h1>Hello, <%- contextKey %></h1>'),
+  template: _.template('<h1>Hello, Jim</h1><button><%- _1NOMINATE_BTN %></button> <button><%- _2NOMINATE_BTN %></button>'),
 
-  templateContext: {
-    contextKey: 'world boo'
-  }
+  templateContext: _.pick(textStrings, "_1NOMINATE_BTN", "_2NOMINATE_BTN")
+
 });
 
 var hello = new HelloWorld();
