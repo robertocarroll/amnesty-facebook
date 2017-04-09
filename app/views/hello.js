3fa63b54c99a9aa8faacff5739c642d3f0d16294
robertocarroll <rob@wearethoughtfox.com>
2017-04-09 09:26:38 +0400

    Tweaks to hello template to load correct strings.

--- app/views/hello.js
+++ app/views/hello.js
@@ -1,8 +1,8 @@
 var HelloWorld = Backbone.Marionette.View.extend({
   el: '#app',
-  template: _.template('<h1>Hello, Jim</h1><button><%- _1NOMINATE_BTN %></button> <button><%- _2NOMINATE_BTN %></button>'),
+  template: _.template('<h1><%- _1NOMINATE %></h1><p><%- _1NOMINATE_DESC %></p><button><%- _1NOMINATE_BTN %></button>'),
 
-  templateContext: _.pick(textStrings, "_1NOMINATE_BTN", "_2NOMINATE_BTN")
+  templateContext: _.pick(textStrings, "_1NOMINATE", "_1NOMINATE_DESC", "_1NOMINATE_BTN")
 
 });
 
