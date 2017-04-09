79e780a0eb1cdf171610899583633f79c926d3d8
robertocarroll <rob@wearethoughtfox.com>
2017-04-09 15:53:42 +0400

    Added getuserdata function to person model.

--- app/models/person.js
+++ app/models/person.js
@@ -5,5 +5,22 @@ var person = Backbone.Model.extend({
       name: null,
       email: null,
       status: 0
+    },
+
+    login: function () {
+      this._getuserdata = function (callback) {
+        console.log('_getuserdata called;');
+        /* Here you can assemble a query */
+        FB.api('/me?fields=third_party_id,email,name', function (response) {
+          if (!response || response.error) {
+            callback(true, response.error);
+          } else {
+            console.log('"/me" query success where username is ' + response['name'] + '.', response);
+            callback(null, response);
+          }
+        });
+
+      };
     }
+
 });
