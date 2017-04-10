d4e9a9d65f68a413465495ca7b4fffa6df9aced3
robertocarroll <rob@wearethoughtfox.com>
2017-04-10 08:14:41 +0400

    Removed the function from person model and changed defaults to match response.

--- app/models/person.js
+++ app/models/person.js
@@ -1,26 +1,6 @@
 var person = Backbone.Model.extend({
     defaults: {
-      id: null,
-      third_party_id: null,
+      facebookID: null,
       name: null,
-      email: null,
-      status: 0
-    },
-
-    login: function () {
-      this._getuserdata = function (callback) {
-        console.log('_getuserdata called;');
-        /* Here you can assemble a query */
-        FB.api('/me?fields=third_party_id,email,name', function (response) {
-          if (!response || response.error) {
-            callback(true, response.error);
-          } else {
-            console.log('"/me" query success where username is ' + response['name'] + '.', response);
-            callback(null, response);
-          }
-        });
-
-      };
     }
-
 });
