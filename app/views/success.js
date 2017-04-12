21ed44c9ba48c9cfdb8e02a82cec231d46eb2354
robertocarroll <rob@wearethoughtfox.com>
2017-04-12 15:36:42 +0400

    Added events to the two buttons on success view.

--- app/views/success.js
+++ app/views/success.js
@@ -1,6 +1,20 @@
 var Success = Backbone.Marionette.View.extend({
   el: '#app',
   template: '#success',
-  templateContext: _.pick(textStrings, "_4NOMINATE", "_4NOMINATE_DESC", "_4NOMINATE_BTN", "_4NOMINATE_BTN_2")
+  templateContext: _.pick(textStrings, "_4NOMINATE", "_4NOMINATE_DESC", "_4NOMINATE_BTN", "_4NOMINATE_BTN_2"),
+
+  events: {
+        "click .nominateAgain": "onNominateAgain",
+        "click .tellYourFriends": "onTellFriends"
+    },
+
+    onNominateAgain: function(e) {
+       //how to render a cached view?
+       //do we need to remove the nominated friend from the list or not?
+    },
+
+    onTellFriends: function(e) {
+
+    }
 
 });
