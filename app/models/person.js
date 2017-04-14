8cc4d9f273286b537b0ff88251b94eeeac3f940e
robertocarroll <rob@wearethoughtfox.com>
2017-04-14 09:51:24 +0400

    Get user details including email and add it to a model.

--- app/models/person.js
+++ app/models/person.js
@@ -2,19 +2,37 @@ var Person = Backbone.Model.extend({
     defaults: {
       facebookID: null,
       name: null,
+      email: null
     },
     initialize: function(){},
 
     login: function(){
 
+      var self = this;
+
       FB.login(function(response) {
         if (response.status === 'connected') {
           console.log ("logged in");
           amnestyApp.onFacebookLogin(response);
+          self.getUserDetails();
         } else {
           console.log ("The person is not logged into this app or we are unable to tell.");
         }
-      }, {scope:'public_profile,user_friends,publish_actions', return_scopes:true});
+      }, {scope:'public_profile,email,user_friends,publish_actions', return_scopes:true});
+    },
+
+    getUserDetails: function () {
+      var self = this;
+
+      FB.api('/me?fields=id,name,email,permissions', function(response) {
+        console.log (response);
+          self.set ({
+            "facebookID": response.id,
+            "name": response.name,
+            "email": response.email,
+          });
+          console.log (self);
+      });
     }
 
 });
