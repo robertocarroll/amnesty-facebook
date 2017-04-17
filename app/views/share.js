3a9f69d67fb7aeb688ffa3e6205b43e9ca641919
Paul D. Waite <paul@pauldwaite.co.uk>
2017-04-17 18:30:11 +0100

    Implemented query-string-based i18n dictionary.

--- app/views/share.js
+++ app/views/share.js
@@ -1,7 +1,7 @@
 var Share = Backbone.Marionette.View.extend({
   el: '#app',
   template: '#share',
-  templateContext: _.pick(textStrings, "_1NOMINATE", "_1NOMINATE_DESC", "_3NOMINATE_CALL", "_3NOMINATE_BTN_2"),
+  templateContext: dictionary.pick("_1NOMINATE", "_1NOMINATE_DESC", "_3NOMINATE_CALL", "_3NOMINATE_BTN_2"),
 
   events: {
         "click .shareForm": "onSubmit"
