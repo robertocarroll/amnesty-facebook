dead3f1294760f11b9e0a1354b036344be4ae838
robertocarroll <rob@wearethoughtfox.com>
2017-04-17 12:45:16 +0400

    Changed the el on the hello child view.

--- app/views/hellobtn.js
+++ app/views/hellobtn.js
@@ -1,8 +1,16 @@
 var HelloBtn = Backbone.Marionette.View.extend({
-  el: '#app',
+  el: '#loadedFacebook',
   template: '#hellobtn',
 
   templateContext: _.pick(textStrings, "_1NOMINATE_BTN"),
 
+  events: {
+      'click .btn-login': 'login'
+    },
 
+   login: function () {
+      console.log ("log in button fired");
+      var amnestyUser = new Person();
+      amnestyUser.login();
+    }
 });
