62a74e69a7da28b7b4f27151e78822b59110546b
robertocarroll <rob@wearethoughtfox.com>
2017-04-28 09:57:17 +0400

    Getting user's location from Facebook.

--- app/models/person.js
+++ app/models/person.js
@@ -2,7 +2,8 @@ var Person = Backbone.Model.extend({
     defaults: {
       facebookID: null,
       name: null,
-      email: null
+      email: null,
+      location: null
     },
 
     login: function(){
@@ -20,19 +21,22 @@ var Person = Backbone.Model.extend({
         } else {
           console.log ("The person is not logged into this app or we are unable to tell.");
         }
-      }, {scope:'public_profile,email,user_friends,publish_actions', return_scopes:true});
+      }, {scope:'public_profile,email,user_friends,publish_actions,user_location', return_scopes:true});
     },
 
     getUserDetails: function () {
       var self = this;
 
-      FB.api('/me?fields=id,name,email,permissions', function(response) {
+      FB.api('/me?fields=id,name,email,location,permissions', function(response) {
           self.set ({
             "facebookID": response.id,
             "name": response.name,
             "email": response.email,
+            "location": response.location.name
           });
+          console.log (self.attributes);
       });
+
     }
 
 });
