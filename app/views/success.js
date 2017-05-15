338cc73a82e10684db89c7fd319fdc00dc45738f
robertocarroll <rob@wearethoughtfox.com>
2017-05-15 19:57:21 +0400

    Tweaks and change to local id.

--- app/views/success.js
+++ app/views/success.js
@@ -2,11 +2,6 @@ var Success = Backbone.Marionette.View.extend({
   template: '#success',
   templateContext: dictionary.pick("_4NOMINATE", "_4NOMINATE_DESC", "_4NOMINATE_BTN_2","_4IFRAME"),
 
-  onRender: function() {
-    //amnestyApp.Views.share.destroy();
-    console.log(amnestyApp.Views.share.isDestroyed());
-  },
-
   events: {
         "click .nominateAgain": "onNominateAgain"
     },
